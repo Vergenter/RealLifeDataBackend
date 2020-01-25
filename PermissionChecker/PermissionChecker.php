62db06b98ce306686108f1c2f98f1eee325c5a64
Vergenter <okolok1998@gmail.com>
2020-01-25 13:32:16 +0100

    fixed bug with accesign route with empty url

--- PermissionChecker/PermissionChecker.php
+++ PermissionChecker/PermissionChecker.php
@@ -3,6 +3,9 @@ require_once __DIR__.'//..//Permission//Permission.php';
 
 function CheckPermission($path,$method,$Permissions){
 
+    if(empty($path)){
+        return false;
+    }
     $subpermission=$Permissions;
     foreach ($path as $part) {
         $tmp=$subpermission->getPermission($part);
--- router.php
+++ router.php
@@ -2,12 +2,14 @@
 require_once __DIR__.'//modules//MainModule.php';
 
 function route($path,$method){
-
+    if(empty($path)){
+        return 404;
+    }
     $module=new MainModule();
     foreach ($path as $part) {
         $tmp=$module->getChild($part);
         if(empty($tmp)){
-            return false;
+            return 404;
         }
         $module=reset($tmp);
     }
