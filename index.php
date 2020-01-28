64cac0dd7aa742370d6edee276ab626ab14e69e9
Vergenter <okolok1998@gmail.com>
2020-01-28 13:50:06 +0100

    added headers for preflight requests

--- index.php
+++ index.php
@@ -5,15 +5,24 @@ include 'router.php';
 require __DIR__.'//vendor//autoload.php';
 
 function init($method,$request,$JWT){
-  header('Content-type: application/json',true,http_response_code());
+  header('Content-type: application/json');
+  header('Access-Control-Allow-Origin: http://localhost:8080');
+  if($method=="OPTIONS"){
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 60');
+    header('Access-Control-Allow-Headers: username,password,Content-Type,JWT');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD');
+  } else {
+    mainLogic($method,$request,$JWT);
+  }
+}
+function mainLogic($method,$request,$JWT){
   http_response_code(
     CheckPermission($request,$method,GetPermission($JWT))?
     route($request,$method):
     401
   );
-  //header('Content-type: application/json',true,http_response_code());
 }
-
 init(
   isset($_SERVER['REQUEST_METHOD'])?$_SERVER['REQUEST_METHOD']:"",
   isset($_SERVER['PATH_INFO'])?explode("/", substr($_SERVER['PATH_INFO'], 1)):"",
